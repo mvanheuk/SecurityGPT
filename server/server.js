b36f9f0f5bcb187667acc06f9b987f52bff17ddb
Marc Vanheukelum <marc.vanheukelumII@perrigo.com>
2023-03-27 15:06:47 -0400

    modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -9,8 +9,6 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-let prompt = ``;
-let responseText = ``;
 const openai = new OpenAIApi(configuration);
 
 const app = express()
@@ -26,58 +24,20 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
   try {
-    const promptCheck = `Does the question in quotes following this sentence pertain to Computers, Technology, Data, Cyber Security, Information Security, Programming, or Business? "${req.body.prompt}" Please only respond with Yes or No.`;
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${promptCheck}`,
-      temperature: 0,
-      max_tokens: 3000,
-      top_p: 1,
-      frequency_penalty: 0.5,
-      presence_penalty: 0,
-    });
+    const prompt = req.body.prompt;
 
-    const botResponse = response.data.choices[0].text;
-
-    if (botResponse.toLowerCase() === 'yes') {
-      prompt = req.body.prompt;
-      res.status(200).send({
-        message: 'Please wait while we generate the response...'
-      });
-    } else if (botResponse.toLowerCase() === 'no') {
-      responseText = 'This question cannot be answered by SecurityGPT due to the limitation in the subjects it can speak on.';
-      res.status(200).send({
-        bot: responseText
-      });
-    } else {
-      responseText = 'I am sorry, I did not understand your response.';
-      res.status(200).send({
-        bot: responseText
-      });
-    }
-
-  } catch (error) {
-    console.error(error)
-    res.status(500).send(error || 'Something went wrong');
-  }
-})
-
-app.post('/getResponse', async (req, res) => {
-  try {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${prompt}`,
-      temperature: 0,
-      max_tokens: 3000,
-      top_p: 1,
-      frequency_penalty: 0.5,
-      presence_penalty: 0,
+      temperature: 0, // Higher values means the model will take more risks.
+      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
-    const botResponse = response.data.choices[0].text;
-
     res.status(200).send({
-      bot: botResponse
+      bot: response.data.choices[0].text
     });
 
   } catch (error) {
