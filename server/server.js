da7bc4b307f194a2a2987ee33d2d3a6fb881d8b7
Marc Vanheukelum <marc.vanheukelumII@perrigo.com>
2023-04-15 22:59:10 -0400

    modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -23,21 +23,20 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
   try {
-    const prompt = req.body.prompt;
+    const userMessage = req.body.prompt;
     const model = "gpt-3.5-turbo";
 
     const response = await openai.createChatCompletion({
       model: model,
-      prompt: `${prompt}`,
-      temperature: 0.8, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      messages: [
+        { role: 'system', content: 'You are a helpful assistant.' },
+        { role: 'user', content: userMessage },
+      ],
+      max_tokens: 2000,
     });
 
     res.status(200).send({
-      bot: response.data.choices[0].text
+      bot: response.choices[0].message.content
     });
 
   } catch (error) {
@@ -48,3 +47,11 @@ app.post('/', async (req, res) => {
 
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
 
+
+// model: model,
+//       prompt: `${prompt}`,
+//       temperature: 0.8, // Higher values means the model will take more risks.
+//       max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+//       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+//       frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+//       presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
