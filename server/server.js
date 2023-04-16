1434afc0f0fed428fb41b952c95309f3d7351064
Marc Vanheukelum <marc.vanheukelumII@perrigo.com>
2023-04-16 19:30:00 -0400

    modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -24,13 +24,28 @@ app.get('/', async (req, res) => {
 app.post('/', async (req, res) => {
   try {
     const userMessage = req.body.prompt;
+    const context = req.body.context;
     const model = "gpt-3.5-turbo";
+
+    // Separate the context into individual messages
+    const messages = context.split('\n').map(content => {
+      const [role, ...messageParts] = content.split(' ');
+      const messageContent = messageParts.join(' ');
+
+      return {
+        role: role.slice(0, -1).toLowerCase(),
+        content: messageContent,
+      };
+    });
+
+    // Add the latest user message to the messages array
+    messages.push({ role: 'user', content: userMessage });
 
     const response = await openai.createChatCompletion({
       model: model,
       messages: [
         { role: 'system', content: 'You are a helpful Security focused assistant called SecurityGPT.' },
-        { role: 'user', content: userMessage },
+        ...messages,
       ],
       max_tokens: 3500,
     });
@@ -45,7 +60,7 @@ app.post('/', async (req, res) => {
     console.error('Error during API call:', error.message);
     res.status(500).send('Something went wrong');
   }
-})
+});
 
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
 
