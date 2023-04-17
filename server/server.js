793088d60678de8177ea7b6aee8db841fbbc0a1a
Marc Vanheukelum <marc.vanheukelumII@perrigo.com>
2023-04-17 10:23:47 -0400

    modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -42,14 +42,12 @@ app.post('/', async (req, res) => {
     // Add the latest user message to the messages array
     messages.push({ role: 'user', content: userMessage });
 
-    const formattedPrompt = [
-      { role: 'system', content: 'You are a helpful Security focused assistant called SecurityGPT.' },
-      ...messages,
-    ].map(message => `${message.role === 'user' ? 'User' : message.role === 'system' ? 'System' : 'Chatbot'}: ${message.content}`).join('\n');
-    
-    const response = await openai.createCompletion({
+    const response = await openai.createChatCompletion({
       model: model,
-      prompt: formattedPrompt,
+      messages: [
+        { role: 'system', content: 'You are a helpful Security focused assistant called SecurityGPT.' },
+        ...messages,
+      ],
       max_tokens: 3500,
     });
 
@@ -66,12 +64,3 @@ app.post('/', async (req, res) => {
 });
 
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
-
-
-// model: model,
-//       prompt: `${prompt}`,
-//       temperature: 0.8, // Higher values means the model will take more risks.
-//       max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-//       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-//       frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-//       presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
