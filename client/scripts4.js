46ca9e905a98d01a4756861caabd22028f74e7e7
mvanheuk <52704745+mvanheuk@users.noreply.github.com>
2023-04-18 19:17:17 -0400

    g

--- client/scripts4.js
+++ client/scripts4.js
@@ -145,7 +145,7 @@ const handleSubmit = async (e) => {
 form.addEventListener('submit', handleSubmit);
 
 const clearButton = document.getElementById('clear_button');
-clearButton.addEventListener('button', clearChat);
+clearButton.addEventListener('click', clearChat);
 
 form.addEventListener('keyup', (e) => {
     if (e.keyCode === 13) {
