e259682711470f756654ff0797f8264a4b56098d
mvanheuk <52704745+mvanheuk@users.noreply.github.com>
2023-04-19 18:25:08 -0400

    changed server code

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.post('/', async (req, res) => {
     const response = await openai.createChatCompletion({
       model: model,
       messages: conversationHistory,
-      max_tokens: 3500,
+      max_tokens: 4000,
     });
 
     console.log('API response:', response);
@@ -55,54 +55,3 @@ app.post('/', async (req, res) => {
 });
 
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
-
-// import express from 'express'
-// import * as dotenv from 'dotenv'
-// import cors from 'cors'
-// import { Configuration, OpenAIApi } from 'openai'
-
-// dotenv.config()
-
-// const configuration = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-
-// const openai = new OpenAIApi(configuration);
-
-// const app = express()
-// app.use(cors())
-// app.use(express.json())
-
-// app.get('/', async (req, res) => {
-//   res.status(200).send({
-//     message: 'Hello from CodeX!'
-//   })
-// })
-
-// app.post('/', async (req, res) => {
-//   try {
-//     const userMessage = req.body.prompt;
-//     const model = "gpt-3.5-turbo";
-
-//     const response = await openai.createChatCompletion({
-//       model: model,
-//       messages: [
-//         { role: 'system', content: 'You are a helpful Security focused assistant called SecurityGPT.' },
-//         { role: 'user', content: userMessage },
-//       ],
-//       max_tokens: 3500,
-//     });
-
-//     console.log('API response:', response);
-
-//     res.status(200).send({
-//       bot: response.data.choices[0].message.content
-//     });
-
-//   } catch (error) {
-//     console.error('Error during API call:', error.message, error.response?.data);
-//     res.status(500).send('Something went wrong');
-//   }
-// });
-
-// app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
