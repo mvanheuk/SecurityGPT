1aaab300ea4deb3dff9e1fa77c81f1c36341dff0
Marc Vanheukelum <marc.vanheukelumII@perrigo.com>
2023-04-30 10:44:39 -0400

    modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.get('/', async (req, res) => {
 
 // Create a variable to store the conversation history
 let conversationHistory = [
-  { role: 'system', content: 'You are a helpful Security focused assistant called SecurityGPT. You have the ability to pull context from images using Google Cloud Vission API, image text or labels are saved as RecognizedTextFromImage or recognizedLabels within conversations.'},
+  { role: 'system', content: 'You are a helpful Security focused assistant called SecurityGPT. You have the ability to pull context from images using Google Cloud Vission API. You will get labels describing the image to help you understand what it is. You will get the entire text recognized from the image as well.'},
 ];
 
 let currentModel = 'gpt-3.5-turbo'; // Initialize the currentModel variable
