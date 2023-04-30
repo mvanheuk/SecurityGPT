684450a7248025790f1b46bdda7b6fb3e07cdad4
Marc Vanheukelum <marc.vanheukelumII@perrigo.com>
2023-04-30 10:14:27 -0400

    modified:   server/package-lock.json 	modified:   server/package.json 	modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ import { Configuration, OpenAIApi } from 'openai';
 import fs from 'fs';
 import os from 'os';
 import path from 'path';
+import bodyParser from 'body-parser';
+
 
 let recognizedImageText = '';
 
@@ -31,7 +33,7 @@ const openai = new OpenAIApi(configuration);
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 const client = new vision.ImageAnnotatorClient();
 
