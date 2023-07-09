bfba26b964ecb8345ba56331a2c703700cf62399
Marc Vanheukelum <marc.vanheukelumII@perrigo.com>
2023-07-09 13:47:47 -0400

    modified:   client/scripts4.js

--- client/scripts4.js
+++ client/scripts4.js
@@ -72,66 +72,29 @@ function loader(element) {
 
 function typeText(element, text) {
   let index = 0;
-  let isCodeBlock = false;
-
+  
   const typeCharacter = () => {
       const currentChar = text[index++];
-
-      // Check if we are entering or exiting a code block
-      if (currentChar === '<' && text.slice(index, index + 5) === 'code>') {
-          isCodeBlock = true;
-      } else if (currentChar === '<' && text.slice(index, index + 7) === '/code>') {
-          isCodeBlock = false;
-      }
-
-      // If current character is a '<', then we need to type out the entire HTML tag at once
-      if (currentChar === '<') {
-          const endOfTag = text.indexOf('>', index);
-          const tag = text.slice(index - 1, endOfTag + 1);
-          index = endOfTag;
-
-          // If the tag is a preformatted text block, type out the entire block at once
-          if (tag === '<pre>') {
-              const endOfBlock = text.indexOf('</pre>', index);
-              const block = text.slice(index, endOfBlock + 6);
-              index = endOfBlock + 6;
-
-              element.lastElementChild.innerHTML += block;
-          } else {
-              // For other tags, add the entire tag to the last child
-              if (!element.lastElementChild || element.lastElementChild.tagName !== 'P') {
-                  element.appendChild(document.createElement('p'));
-              }
-              element.lastElementChild.innerHTML += tag;
-          }
-      }
-      // Handle newline characters
-      else if (currentChar === '\n') {
-          // Only create a new paragraph if the last child is not a preformatted text block
-          if (!isCodeBlock && element.lastElementChild && element.lastElementChild.tagName !== 'PRE') {
-              element.appendChild(document.createElement('p'));
-          } else if (isCodeBlock) {
-              element.lastElementChild.textContent += '\n';
-          }
-      }
-      // Normal character typing
-      else {
-          if (!element.lastElementChild || element.lastElementChild.tagName !== 'P') {
-              element.appendChild(document.createElement('p'));
-          }
-          element.lastElementChild.innerHTML += currentChar;
-      }
-
-      chatContainer.scrollTop = chatContainer.scrollHeight - chatContainer.clientHeight;
-
-      if (index < text.length) {
-          requestAnimationFrame(typeCharacter);
+        
+      if (currentChar === '\n') {
+         element.appendChild(document.createElement('p'));
       } else {
-          Prism.highlightAllUnder(element);
+         if (!element.lastElementChild || element.lastElementChild.tagName !== 'P') {
+             element.appendChild(document.createElement('p'));
+         }
+         element.lastElementChild.innerHTML += currentChar;
       }
-  };
+        
+      chatContainer.scrollTop = chatContainer.scrollHeight - chatContainer.clientHeight;
+      
+      if(index < text.length){
+        requestAnimationFrame(typeCharacter);
+      } else {
+        Prism.highlightAll();
+      }
+   };
 
-  requestAnimationFrame(typeCharacter);
+   requestAnimationFrame(typeCharacter);
 }
 
 function escapeHtml(unsafe) {
@@ -149,6 +112,7 @@ function chatStripe(isAi, value, uniqueId, imageBase64) {
   let isCode = false;
   let language = null;
 
+  // Check if value is a code block 
   if (value.trim().startsWith("```") && value.trim().endsWith("```")) {
     value = value.trim().slice(3, -3); // Trim the backticks
     const firstLineEndIndex = value.indexOf("\n");
@@ -157,7 +121,7 @@ function chatStripe(isAi, value, uniqueId, imageBase64) {
     isCode = true;
   }
 
-  const valueMarkup = isCode ? `<pre><code class="language-${language}">${value}</code></pre>` : value;
+  const valueMarkup = isCode ? `<pre><code class="language-${language}">${escapeHtml(value).replace(/\n/g, '<br>')}</code></pre>` : value;
 
   // to focus scroll to the bottom here
   chatContainer.scrollTop = chatContainer.scrollHeight - chatContainer.clientHeight;
@@ -180,6 +144,45 @@ function chatStripe(isAi, value, uniqueId, imageBase64) {
     `
   );
 }
+
+// function chatStripe(isAi, value, uniqueId, imageBase64) {
+//   const imageMarkup = imageBase64 ? `<img src="data:image/jpeg;base64,${imageBase64}" class="uploaded-image" />` : '';
+
+//   let isCode = false;
+//   let language = null;
+
+//   // Check if value is a code block 
+//   if (value.trim().startsWith("```") && value.trim().endsWith("```")) {
+//     value = value.trim().slice(3, -3); // Trim the backticks
+//     const firstLineEndIndex = value.indexOf("\n");
+//     language = value.slice(0, firstLineEndIndex).trim(); // Get the language from the first line
+//     value = value.slice(firstLineEndIndex + 1); // Get the rest of the string after the first line
+//     isCode = true;
+//   }
+
+//   const valueMarkup = isCode ? `<pre><code class="language-${language}">${escapeHtml(value)}</code></pre>` : value;
+
+//   // to focus scroll to the bottom here
+//   chatContainer.scrollTop = chatContainer.scrollHeight - chatContainer.clientHeight;
+//   return (
+//     `
+//     <div class="wrapper ${isAi && 'ai'}">
+//         <div class="chat">
+//             <div class="profile">
+//                 <img 
+//                   src=${isAi ? bot : user} 
+//                   alt="${isAi ? 'bot' : 'user'}" 
+//                 />
+//             </div>
+//             <div class="message" id=${uniqueId}>
+//                 ${imageMarkup}
+//                 ${valueMarkup}
+//             </div>
+//         </div>
+//     </div>
+//     `
+//   );
+// }
 
 async function clearChat() {
     // Clear the chat container
@@ -309,34 +312,14 @@ const handleSubmit = async (e) => {
 
     //Clear % value
     progressPercentage.textContent = ``;
-    
+
     if (response.ok) {
-      const data = await response.json();
-      let parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
-      
-      // Regular expression to match code blocks
-      const codeBlockRegex = /```([^`]+)```/g;
-  
-      // Function to replace code blocks with highlighted code blocks
-      const replaceCodeBlock = (match) => {
-          const firstLineEndIndex = match.indexOf("\n");
-          const language = match.slice(3, firstLineEndIndex).trim(); // Get the language from the first line
-          const code = match.slice(firstLineEndIndex + 1, -3).trim(); // Get the rest of the string after the first line
-  
-          // Create a code block with the appropriate markup for Prism
-          return `<pre><code class="language-${language}">${escapeHtml(code)}</code></pre>`;
-      }
-  
-      // Replace all code blocks with highlighted code blocks
-      parsedData = parsedData.replace(codeBlockRegex, replaceCodeBlock);
-  
-      // Split the rest of the response into paragraphs and wrap them in <p> tags
-      const paragraphs = parsedData.split('\n\n').map((paragraph) => `<p>${paragraph}</p>`).join('');
-  
-      // Call typeText function to display bot's response
-      typeText(messageDiv, parsedData);
-    }
-    else {
+        const data = await response.json();
+        const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
+        const paragraphs = parsedData.split('\n\n').map((paragraph) => `<p>${paragraph}</p>`).join('');
+
+        typeText(messageDiv, parsedData);
+    } else {
         const err = await response.text()
 
         messageDiv.innerHTML = "Something went wrong"
